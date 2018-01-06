cc6f3b4ee623fcfe3f4c19661d6ab3d8cda2d9ea
黄紫翊 <1109138356@qq.com>
2018-01-06 17:02:43 +0800

    afad

--- 组长index/liangcang/js/aixin.js
+++ 组长index/liangcang/js/aixin.js
@@ -1,8 +1,46 @@
 //爱心图点击效果、获取商品信息 、反馈给购物车页面
- var oFav=document.querySelectorAll('.aright');console.log(oFav)
-for(var bl=0;bl<oFav.length;bl++){
-		oFav[bl].onclick=(function(){
-			oFav[bl].style.background="url(../img/redlove.png)no-repeat 30px 10px"
-	})(bl)
+ var oFav=document.querySelectorAll('.aright');console.log(oFav[0].style.background);
+ var j=0;
+for(i=0;i<oFav.length;i++){
+	(function(i){
+		oFav[i].onclick=function(){
+			j++;
+			if(j%2!=0){
+				oFav[i].style.background="url(img/redlove.png)no-repeat 30px 10px"
+			}else{
+				oFav[i].style.background=""
+			}
+//ajax获取商品信息
+			var strUsername = oUsername.value
+			var strPassword = oPassword.value
+
+			var json = {
+				"username": strUsername,
+				password: strPassword,
+				status: "login"
+			}
+			 //调用函数
+			loginOrRegister(json, function(obj) {
+				if (obj.code == 0) {
+					alert("登陆成功")
+					
+					// 页面跳转
+					location.href = "https://hzypzy.github.io/liangcang/index.html"
+					
+					
+					
+				} else {
+					alert(obj.message)
+				}
+			})
+				}
+			})(i);
 }
-	
+
+
+
+//		for(var bl=0;bl<oFav.length;bl++){
+//			(function(i){
+//			oFav[i].style.background="url(img/redlove.png)no-repeat 30px 10px"
+//		})(i);
+//	}
---
+++ 组长index/liangcang/js/ajax2.js
@@ -0,0 +1,18 @@
+//获取内容商品图片以及信息
+	var xhr=new XMLHttpRequest();
+	xhr.onreadystatechange=function(){
+		console.log(1);
+		if(xhr.readystate==4&&xhr.status==200){
+			var str=xhr.responseText;
+			var obj=JSON.parse(str);
+//			console.log(obj);
+				var arr=[];
+				arr=obj.data;console.log(arr)
+//			for(var i=0;i<){
+//				obj.data[i]
+//			}
+//			
+		}
+	}
+xhr.open("GET","http://csit.top/api_goods.php",true);
+xhr.send();
