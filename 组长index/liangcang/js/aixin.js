b9800b390ef5c346925fe29dd85742a64d147e43
黄紫翊 <1109138356@qq.com>
2018-01-06 20:06:35 +0800

    zz

--- 组长index/liangcang/js/aixin.js
+++ 组长index/liangcang/js/aixin.js
@@ -1,46 +1,52 @@
 //爱心图点击效果、获取商品信息 、反馈给购物车页面
  var oFav=document.querySelectorAll('.aright');console.log(oFav[0].style.background);
- var j=0;
+ var j=true;var l=0;
+// 	for(var l=0;l<oFav.length;l++){
+// 		
+// 	}
 for(i=0;i<oFav.length;i++){
 	(function(i){
+//		oFav[i].style.j=true;
 		oFav[i].onclick=function(){
-			j++;
-			if(j%2!=0){
+			if(i!=l){
+				j=!j
+			};
+			l=i;
+			console.log(i);console.log(j)
+			if(j){
 				oFav[i].style.background="url(img/redlove.png)no-repeat 30px 10px"
+				j=!j;
 			}else{
 				oFav[i].style.background=""
+				j=!j;
 			}
-//ajax获取商品信息
-			var strUsername = oUsername.value
-			var strPassword = oPassword.value
-
-			var json = {
-				"username": strUsername,
-				password: strPassword,
-				status: "login"
-			}
-			 //调用函数
-			loginOrRegister(json, function(obj) {
-				if (obj.code == 0) {
-					alert("登陆成功")
-					
-					// 页面跳转
-					location.href = "https://hzypzy.github.io/liangcang/index.html"
-					
-					
-					
-				} else {
-					alert(obj.message)
-				}
-			})
-				}
-			})(i);
+		}
+	})(i);
 }
 
 
 
-//		for(var bl=0;bl<oFav.length;bl++){
-//			(function(i){
-//			oFav[i].style.background="url(img/redlove.png)no-repeat 30px 10px"
-//		})(i);
-//	}
+
+//ajax获取商品信息
+//			var strUsername = oUsername.value
+//			var strPassword = oPassword.value
+//
+//			var json = {
+//				"username": strUsername,
+//				password: strPassword,
+//				status: "login"
+//			}
+			 //调用函数
+//			loginOrRegister(json, function(obj) {
+//				if (obj.code == 0) {
+//					alert("登陆成功")
+//					
+//					// 页面跳转
+//					location.href = "https://hzypzy.github.io/liangcang/index.html"
+//					
+//					
+//					
+//				} else {
+//					alert(obj.message)
+//				}
+//			})
--- 组长index/liangcang/js/ajax2.js
+++ 组长index/liangcang/js/ajax2.js
@@ -1,11 +1,11 @@
 //获取内容商品图片以及信息
 	var xhr=new XMLHttpRequest();
 	xhr.onreadystatechange=function(){
-		console.log(1);
+//		console.log(1);
 		if(xhr.readystate==4&&xhr.status==200){
 			var str=xhr.responseText;
 			var obj=JSON.parse(str);
-//			console.log(obj);
+			console.log(obj);
 				var arr=[];
 				arr=obj.data;console.log(arr)
 //			for(var i=0;i<){
